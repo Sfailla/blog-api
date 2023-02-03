31994b371d98ecafb2372946c905ef8492091d20
Steven-failla_mmcgh <steven.failla@nera.com>
2023-02-03 14:40:37 -0500

    formatting changes

--- vendor/terminalColors.js
+++ vendor/terminalColors.js
@@ -1,44 +1,44 @@
 // cool function I made for development to change terminal colors ✨🌈✨
 const colorTerminal = terminalColor => {
-  terminalColor = terminalColor.toLowerCase()
-  const acceptedColors = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan']
+	terminalColor = terminalColor.toLowerCase()
+	const acceptedColors = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan']
 
-  const checkColorReturnCode = color => {
-    const colorMap = {
-      red: 31,
-      green: 32,
-      yellow: 33,
-      blue: 34,
-      magenta: 35,
-      cyan: 36
-    }
+	const checkColorReturnCode = color => {
+		const colorMap = {
+			red: 31,
+			green: 32,
+			yellow: 33,
+			blue: 34,
+			magenta: 35,
+			cyan: 36
+		}
 
-    return colorMap[color]
-  }
+		return colorMap[color]
+	}
 
-  const verifiedColor = acceptedColors.find(color => color === terminalColor)
+	const verifiedColor = acceptedColors.find(color => color === terminalColor)
 
-  const colorModel = acceptedColors.reduce((obj, items) => {
-    if (verifiedColor) {
-      obj[items] = {
-        isColor: items === verifiedColor,
-        color: items,
-        colorCode: checkColorReturnCode(items)
-      }
-    } else {
-      obj['error'] = {
-        isColor: false,
-        message: `sorry ${terminalColor} is not available 😕\n`
-      }
-    }
-    return obj
-  }, {})
+	const colorModel = acceptedColors.reduce((obj, items) => {
+		if (verifiedColor) {
+			obj[items] = {
+				isColor: items === verifiedColor,
+				color: items,
+				colorCode: checkColorReturnCode(items)
+			}
+		} else {
+			obj['error'] = {
+				isColor: false,
+				message: `sorry ${terminalColor} is not available at this time 🙁`
+			}
+		}
+		return obj
+	}, {})
 
-  const model = verifiedColor ? colorModel[verifiedColor] : colorModel['error']
+	const model = verifiedColor ? colorModel[verifiedColor] : colorModel['error']
 
-  const { colorCode, message } = model
+	const { colorCode, message } = model
 
-  return colorCode ? `\x1b[${colorCode}m%s\x1b[0m` : message
+	return colorCode ? `\x1b[${colorCode}m%s\x1b[0m` : message
 }
 
 module.exports = colorTerminal
