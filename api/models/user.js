241a54c07df5b38b17a0edc43a8f3d550182099f
Steven-failla_mmcgh <steven.failla@nera.com>
2023-02-04 08:27:55 -0500

    formatting change

--- api/models/user.js
+++ api/models/user.js
@@ -5,52 +5,52 @@ const { Schema, model } = mongoose
 
 const typeProps = { trim: true, unique: true, index: true }
 const requiredString = {
-  type: String,
-  required: [true, 'must provide field']
+	type: String,
+	required: [true, 'must provide field']
 }
 
 const validateEmail = {
-  validator: value => {
-    return validator.isEmail(value)
-  },
-  message: '{VALUE} is not a valid email address'
+	validator: value => {
+		return validator.isEmail(value)
+	},
+	message: '{VALUE} is not a valid email address'
 }
 
 const UserSchema = new Schema(
-  {
-    username: { ...requiredString, ...typeProps },
-    email: {
-      ...requiredString,
-      ...typeProps,
-      lowercase: true,
-      validate: validateEmail
-    },
-    password: { ...requiredString, trim: true },
-    role: {
-      type: String,
-      enum: ['user', 'moderator', 'admin'],
-      default: 'user'
-    },
-    createdAt: { type: Date, default: Date.now }
-  },
-  {
-    toJSON: {
-      virtuals: true,
-      versionKey: false,
-      transform: (_, ret) => {
-        delete ret._id
-        delete ret.password
-      }
-    }
-  }
+	{
+		username: { ...requiredString, ...typeProps },
+		email: {
+			...requiredString,
+			...typeProps,
+			lowercase: true,
+			validate: validateEmail
+		},
+		password: { ...requiredString, trim: true },
+		role: {
+			type: String,
+			enum: ['user', 'moderator', 'admin'],
+			default: 'user'
+		},
+		createdAt: { type: Date, default: Date.now }
+	},
+	{
+		toJSON: {
+			virtuals: true,
+			versionKey: false,
+			transform: (_, ret) => {
+				delete ret._id
+				delete ret.password
+			}
+		}
+	}
 )
 
 UserSchema.post('save', function (error, doc, next) {
-  if (error.name === 'MongoError' && error.code === 11000) {
-    next(new ValidationError(400, 'username or email already exists'))
-  } else {
-    next(error)
-  }
+	if (error.name === 'MongoError' && error.code === 11000) {
+		next(new ValidationError(400, 'username or email already exists'))
+	} else {
+		next(error)
+	}
 })
 
 module.exports = model('User', UserSchema)
