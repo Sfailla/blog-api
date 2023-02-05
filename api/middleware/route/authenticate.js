54558043eb688f7d6707db91693bd37832eaaf9b
Steven-failla_mmcgh <steven.failla@nera.com>
2023-02-04 19:55:39 -0500

    improve use of error handlers, formatting

--- api/middleware/route/authenticate.js
+++ api/middleware/route/authenticate.js
@@ -2,27 +2,27 @@ const { verifyToken, makeAuthUser } = require('../../helpers/user-auth')
 const { ValidationError, TokenExpiredError } = require('../utils/errors')
 const UserModel = require('../../models/user')
 
-const required = async (req, res, next) => {
-  try {
-    const token = req.header('x-auth-token')
-    const verifiedUser = await verifyToken(token, process.env.ACCESS_TOKEN_SECRET)
+const required = async (req, _res, next) => {
+	try {
+		const token = req.header('x-auth-token')
+		const verifiedUser = await verifyToken(token, process.env.ACCESS_TOKEN_SECRET)
 
-    if (!verifiedUser) {
-      const errMsg = 'error verifying user token in validation middleware'
-      throw new TokenExpiredError(400, errMsg)
-    }
+		if (!verifiedUser) {
+			const errMsg = 'error verifying user token in validation middleware'
+			throw new ValidationError(400, errMsg)
+		}
 
-    const user = await UserModel.findById(verifiedUser.userId)
-    req.user = makeAuthUser(user)
+		const user = await UserModel.findById(verifiedUser.userId)
+		req.user = makeAuthUser(user)
 
-    await next()
-  } catch (error) {
-    if (error.name === 'TokenExpiredError' || error.name === 'JsonWebTokenError') {
-      error.status = 403
-      await next(error)
-    }
-    await next(error)
-  }
+		await next()
+	} catch (error) {
+		if (error.name === 'TokenExpiredError' || error.name === 'JsonWebTokenError') {
+			const tokenError = new TokenExpiredError(403, 'token expired')
+			await next(tokenError)
+		}
+		await next(error)
+	}
 }
 
 module.exports = { required }
